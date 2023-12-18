36c6af1d7fb53a41408f520a69b2d4a01eb56503
Muhammad Zidane Alrizky <zidanealrizky@gmail.com>
2023-12-18 20:56:17 +0700

    menambahkan fitur chart dan search

--- js/script.js
+++ js/script.js
@@ -1,15 +1,52 @@
-// toggle class active
+// toggle class active hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 // ketika hamburger menu diklik
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
+// toggle class active search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// toggle class active  untuk shopping cart
+const shoppingCart = document.querySelector(".shopping-cart");
+
+// ketika shopping cart menu diklik
+document.querySelector("#shopping-cart").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
 
 // klik diluar sidebar untuk menghilangkan navnya
 const hamburger = document.querySelector("#hamburger-menu");
+const searchButton = document.querySelector("#search-button");
+const shoppingButton = document.querySelector("#shopping-cart");
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+  if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+  if (!shoppingButton.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
 });
+
+// modal box
+
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButton = document.querySelector(".item-detail-btn");
+
+// membuat function css menjadi display flex
+itemDetailButton.onclick = (e) => {
+  itemDetailModal.style.display = "flex";
+  e.preventDefault();
+};
